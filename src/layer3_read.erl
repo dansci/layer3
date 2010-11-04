937243571e0f563eac6351fcf6088fec84839cca
Daniel Scislowski <dansci@colibri.localdomain>
2010-11-04 11:26:50 -0700

    Added functions to parse the data structure hmhj_layer2 will pass in layer3_read.erl.  Currently only have support for all channels on a single card.  Plus, I need to turn it into JSON.

--- src/layer3_read.erl
+++ src/layer3_read.erl
@@ -8,14 +8,33 @@ init([]) ->
     {ok, undefined}.
 
 to_html(ReqData, State) ->
+    %% FIXME: may break when card or channel wasn't specified in URI
     Card = wrq:path_info(card, ReqData),
     Channel = wrq:path_info(channel, ReqData),
+    
+    %% FIXME: need support for multiple cards
     hmhj_layer2:process_request(
       #l2request{from=self(), to=Card, action=read}),
     
     receive {R, BinData} ->
 	    Data = binary_to_term(BinData)
     end,
+
+    {Tstamp, VoltageList} = Data,
+    %% Coerce timestamp
+    {Ms, S, _us} = Tstamp,
+    Timestamp = 1000000*Ms + S,
+    %% Coerce voltages
+    NumberedVoltages = lists:zip(lists:seq(1, length(VoltageList)),
+				 VoltageList),
+    ChannelDS = lists:map(fun structify_channels/1,
+			  NumberedVoltages),
+
+    %% FIXME: need support for picking out a channel
     
     {"<html>Data was read.  It'll be listed here soon... </html>",
      ReqData, State}.
+
+structify_channels({N, V}) ->
+    {list_to_binary("channel" ++ integer_to_list(N)),
+     {struct, [{<<"voltage">>, V}]}}.
