f019da2ddafd2d9f61409aa288b5c5ea5059f53a
dansci <dansci@ioserver.(none)>
2010-12-02 11:00:54 -0800

    Code changes for being on the IO server

---
+++ ebin/hmhj_layer3.app
@@ -0,0 +1,25 @@
+%%-*- mode: erlang -*-
+{application, hmhj_layer3,
+ [
+  {description, "hmhj_layer3"},
+  {vsn, "1"},
+  {modules, [
+             hmhj_layer3,
+             hmhj_layer3_app,
+             hmhj_layer3_sup,
+             hmhj_layer3_resource,
+	     hmhj_layer3_read,
+	     hmhj_layer3_tools
+            ]},
+  {registered, []},
+  {applications, [
+                  kernel,
+                  stdlib,
+                  crypto,
+                  mochiweb,
+                  webmachine,
+		  hmhj_layer2
+                 ]},
+  {mod, { hmhj_layer3_app, []}},
+  {env, []}
+ ]}.
---
+++ src/hmhj_layer3_tools.erl
@@ -0,0 +1,50 @@
+-module(hmhj_layer3_tools).
+
+-export([struct_to_json/1]).
+
+struct_to_json([]) ->
+    "";
+
+struct_to_json({struct, [{Key, Val}]}) ->
+    "{" ++ "\"" ++ atom_to_list(Key) ++ "\"" ++  ":" ++
+	struct_to_json(Val) ++ "}";
+
+struct_to_json({struct, [{Key, Val} | Rest]}) ->
+    "{" ++ "\"" ++ atom_to_list(Key) ++ "\"" ++  ":" ++
+	struct_to_json(Val) ++ "," ++ struct_to_json(Rest) ++ "}";
+
+struct_to_json([{Key, Val}]) ->
+    "\"" ++ atom_to_list(Key) ++ "\"" ++ ":" ++
+	struct_to_json(Val);
+
+struct_to_json([{Key, Val} | Rest]) ->
+    struct_to_json([{Key, Val}]) ++"," ++ struct_to_json(Rest);
+
+struct_to_json({Key, Val}) ->
+    "\"" ++ atom_to_list(Key) ++ "\"" ++ ":" ++
+	struct_to_json(Val);
+
+struct_to_json(Val) when is_integer(Val) ->
+    integer_to_list(Val);
+struct_to_json(Val) when is_float(Val) ->
+    float_to_list(Val);
+struct_to_json(Val) when is_binary(Val) ->
+    binary_to_list(Val);
+struct_to_json(Val) when is_atom(Val) ->
+    atom_to_list(Val).
+
+
+
+
+%% To test this stupid parser:
+
+%% DS = {struct, [{<<"cardA">>,
+%% 		{struct, [{<<"channel1">>,
+%% 			   {struct, [{<<"voltage">>, 8.2}]}},
+%% 			  {<<"timestamp">>, 128988392},
+%% 			  {<<"channel2">>,
+%% 			   {struct, [{<<"voltage">>, 2.3}]}}]}},
+%% 	       {<<"NTPLink">>, true},
+%% 	       {<<"cardB">>,
+%% 		{struct, [{<<"timestamp">>, 128988390}]}}]}.
+      
