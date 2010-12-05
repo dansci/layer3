ee270f9511ce25bc66cf3aa80523d089e50f0b8d
dansci <dansci@ioserver.(none)>
2010-12-04 16:38:30 -0800

    updated TODO and added some test cases to test_layer.

--- test_layer.sh
+++ test_layer.sh
@@ -6,9 +6,12 @@ curl -s -i http://localhost:8000/test
 # get all data from cardA
 curl -s -i http://localhost:8000/data/cardA/
 
+# and from cardB
+curl -s -i http://localhost:8000/data/cardB/
+
 # get channel 2 from cardA
 curl -s -i http://localhost:8000/data/cardA/channel2
 
-
+# configure some legit stuff on cards A and B
 curl -vvv -s -X POST -H "Content-Type: application/json" \
-    -d '{"cardA":{"gain": 2, "mode":"single_ended", "channel1":{"gain":4}, "channel2":{"gain":8}}, "cardB":{"mode":"single_ended", "channel4":{"gain":4}}}' http://localhost:8000/config
+    -d '{"cardA":{"gain": 2, "mode":"differential", "channel1":{"gain":4}, "channel2":{"gain":8}}, "cardB":{"mode":"differential", "channel4":{"gain":4}}}' http://localhost:8000/config
